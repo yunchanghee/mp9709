fff9aff0506b57a424e9934baa7f342a36d94e72
mp9709 <mp9709@naver.com>
2016-06-27 10:55:03 -0700

    yyy

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-	asfdasdfdsfafasdfdsdsfsdfsdfsdfsadf
+	8708989786867
 	}
 }
